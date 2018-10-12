baa7af5d2c75548ae61771123a61c55a9cb72125
Daniel_massato_shiroma <Daniel_massato_shiroma@hotmail.com>
2018-10-12 17:33:44 -0300

    Update

---
+++ ProjetoModuloDDD.Domain/Entities/Ciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjetoModuloDDD.Domain.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            return 
+        }
+
+    }
+}
---
+++ ProjetoModuloDDD.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetoModuloDDD.MVC.MvcApplication" Language="C#" %>
